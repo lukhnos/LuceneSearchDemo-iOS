9c4f26b623c20201efccbb4fadc33737c9e369b0
Lukhnos Liu <lukhnos@lukhnos.org>
2015-08-31 01:49:59 -0700

    Provide dependency setup script

---
+++ setup-j2objc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+# TODO: Parameterize, error checking, temp file, etc.
+URL=https://github.com/lukhnos/j2objc/releases/download/0.9.8.1-20150829-2c1855d/j2objc-0.9.8.1-20150829-2c1855d.zip
+FILE=j2objc-0.9.8.1-20150829-2c1855d.zip
+DIR=j2objc-0.9.8.1-20150829-2c1855d
+TARGET=vendor
+
+echo Fetching ${URL}
+curl -L -o "${FILE}" "${URL}"
+
+echo Creating dir ${TARGET}
+mkdir -p "${TARGET}"
+
+echo Unzipping ${FILE}
+unzip "${FILE}"
+
+echo Moving ${DIR} to ${TARGET}/j2objc
+mv "${DIR}" "${TARGET}/j2objc"
